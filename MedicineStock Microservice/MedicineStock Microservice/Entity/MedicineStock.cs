0ecba6a816c890cb57a46b10db9a804d4f8ea477
manishjayan <manishjayan@gmail.com>
2021-04-14 19:52:09 +0530

    Requirement 1 and 2 Uploded to git

---
+++ MedicineStock Microservice/MedicineStock Microservice/Entity/MedicineStock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+namespace MedicineStock_Microservice.Entity
+{
+    public class MedicineStock
+    {
+        [Key]
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(250)]
+        public string ChemicalComposition { get; set; }
+        public string TargetAilment { get; set; }
+        public DateTime DateOfExpiry { get; set; }
+        public int NumberOfTabletsInStock { get; set; }
+    }
+}
---
+++ MedicineStock Microservice/MedicineStock Microservice/Entity/MedicineStockContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicineStock_Microservice.Entity
+{
+    public class MedicineStockContext:DbContext
+    {
+       
+            public MedicineStockContext(DbContextOptions<MedicineStockContext> option) : base(option)
+            {
+
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Seed();
+            }
+
+            public DbSet<MedicineStock> MedicineStock { get; set; }
+        
+    }
+}
