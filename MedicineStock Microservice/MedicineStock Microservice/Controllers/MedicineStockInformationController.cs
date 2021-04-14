9a0314d86ca7eb7e066ef1bf14a720b3cd9f2c19
manishjayan <manishjayan@gmail.com>
2021-04-14 22:26:08 +0530

    Example to git push

--- MedicineStock Microservice/MedicineStock Microservice/Controllers/MedicineStockInformationController.cs
+++ MedicineStock Microservice/MedicineStock Microservice/Controllers/MedicineStockInformationController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MedicineStock_Microservice.Entity;
 
+// commend git
 namespace MedicineStock_Microservice.Controllers
 {
     [Route("api/[controller]")]
