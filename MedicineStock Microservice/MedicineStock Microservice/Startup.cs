e7a4e7da1272333be1a6ba015dfe634e72a82d34
manishjayan <manishjayan@gmail.com>
2021-04-15 16:25:31 +0530

    Removed Entity Framework Dependency

--- MedicineStock Microservice/MedicineStock Microservice/Startup.cs
+++ MedicineStock Microservice/MedicineStock Microservice/Startup.cs
@@ -2,7 +2,6 @@ using MedicineStock_Microservice.Entity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
