70152f7e2e5514de0c141267e8f07f31723c6fc0
manishjayan <manishjayan@gmail.com>
2021-04-15 16:21:59 +0530

    Removed Entity Framework Dependency

--- MedicineStock Microservice/MedicineStock Microservice/Controllers/MedicineStockInformationController.cs
+++ MedicineStock Microservice/MedicineStock Microservice/Controllers/MedicineStockInformationController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using MedicineStock_Microservice.Entity;
 
 namespace MedicineStock_Microservice.Controllers
