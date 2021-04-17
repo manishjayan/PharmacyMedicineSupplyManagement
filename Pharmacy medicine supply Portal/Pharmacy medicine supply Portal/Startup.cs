862b1760b5418e94f2960788084ddb4664811e77
manishjayan <manishjayan@gmail.com>
2021-04-17 12:57:49 +0530

    Adding all Jishnus Api

--- Pharmacy medicine supply Portal/Pharmacy medicine supply Portal/Startup.cs
+++ Pharmacy medicine supply Portal/Pharmacy medicine supply Portal/Startup.cs
@@ -1,19 +1,14 @@
-using MedicineStock_Microservice.Entity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MedicineStock_Microservice
+namespace Pharmacy_medicine_supply_Portal
 {
     public class Startup
     {
@@ -27,15 +22,8 @@ namespace MedicineStock_Microservice
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MedicineStock_Microservice", Version = "v1" });
-            });
-            
-           
+            services.AddControllersWithViews();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,17 +32,24 @@ namespace MedicineStock_Microservice
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MedicineStock_Microservice v1"));
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseSession();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
