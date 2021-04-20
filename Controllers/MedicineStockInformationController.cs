ac15eded81691c12f9056cbb6b3dfa105f2b16c2
manishjayan <manishjayan@gmail.com>
2021-04-20 15:42:33 +0530

    Added Repository Pattern

--- Controllers/MedicineStockInformationController.cs
+++ Controllers/MedicineStockInformationController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using MedicineStock_Microservice.Entity;
+using MedicineStock_Microservice.DAL;
 
 namespace MedicineStock_Microservice.Controllers
 {
@@ -13,32 +14,17 @@ namespace MedicineStock_Microservice.Controllers
     [ApiController]
     public class MedicineStockInformationController : ControllerBase
     {
-    
-        List<MedicineStock> medicineStock=new List<MedicineStock>();
-        public MedicineStockInformationController()
-        {
-           
-            medicineStock.AddRange(new List<MedicineStock>
-            {
-                  new MedicineStock() { Name = "Orthoherb",ChemicalComposition= "Castor Plant,Adulsa,Neem",TargetAilment= "Orthopaedics",DateOfExpiry=Convert.ToDateTime("2022-12-31"),NumberOfTabletsInStock=10000 },
-                  new MedicineStock() {Name = "Cholecalciferol", ChemicalComposition = "aspartame ,food dyes", TargetAilment = "Orthopaedics", DateOfExpiry = Convert.ToDateTime("2023-1-1") ,NumberOfTabletsInStock=5000},
-                  new MedicineStock() {Name = "Gaviscon", ChemicalComposition= "sodium alginate, sodium bicarbonate", TargetAilment= "General", DateOfExpiry=Convert.ToDateTime("2022-12-31"),NumberOfTabletsInStock=4000 },
-                  new MedicineStock() { Name = "Dolo-650", ChemicalComposition = "Acetaminophen , Dexbrompheniramine", TargetAilment = "General", DateOfExpiry = Convert.ToDateTime("2022-12-31"),NumberOfTabletsInStock=8000 },
-                  new MedicineStock() { Name = "Cyclopam", ChemicalComposition = "Dicyclomine  , Paracetamol ", TargetAilment = "Gynaecology", DateOfExpiry = Convert.ToDateTime("2022-12-31") ,NumberOfTabletsInStock=12000},
-                  new MedicineStock() { Name = "Hilact", ChemicalComposition = "Asparagus racemosus  , Anethum sowa ", TargetAilment = "Gynaecology", DateOfExpiry = Convert.ToDateTime("2022-12-31"),NumberOfTabletsInStock=6000 }
 
-            });
-        }
-
+        MedicineStokeRespo respository = new MedicineStokeRespo();
         // GET: api/MedicineStockInformation
         [HttpGet]
         public IEnumerable<MedicineStock> GetMedicineStock()
         {
-          
-            return  medicineStock;
+            //Return List of all MedicineStoke
+            return respository.GetAllStokeDetails();
         }
 
-        
-     
+
+
     }
 }
---
+++ DAL/MedicineStokeRespo.cs
@@ -0,0 +1,30 @@
+﻿using MedicineStock_Microservice.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicineStock_Microservice.DAL
+{
+    public class MedicineStokeRespo
+    {
+        List<MedicineStock> medicineStock = new List<MedicineStock>()
+        {
+
+                 new MedicineStock() { Name = "Orthoherb",ChemicalComposition= "Castor Plant,Adulsa,Neem",TargetAilment= "Orthopaedics",DateOfExpiry=Convert.ToDateTime("2022-12-31"),NumberOfTabletsInStock=10000 },
+                 new MedicineStock() {Name = "Cholecalciferol", ChemicalComposition = "aspartame ,food dyes", TargetAilment = "Orthopaedics", DateOfExpiry = Convert.ToDateTime("2023-1-1") ,NumberOfTabletsInStock=5000},
+                 new MedicineStock() {Name = "Gaviscon", ChemicalComposition= "sodium alginate, sodium bicarbonate", TargetAilment= "General", DateOfExpiry=Convert.ToDateTime("2022-12-31"),NumberOfTabletsInStock=4000 },
+                 new MedicineStock() { Name = "Dolo-650", ChemicalComposition = "Acetaminophen , Dexbrompheniramine", TargetAilment = "General", DateOfExpiry = Convert.ToDateTime("2022-12-31"),NumberOfTabletsInStock=8000 },
+                 new MedicineStock() { Name = "Cyclopam", ChemicalComposition = "Dicyclomine  , Paracetamol ", TargetAilment = "Gynaecology", DateOfExpiry = Convert.ToDateTime("2022-12-31") ,NumberOfTabletsInStock=12000},
+                 new MedicineStock() { Name = "Hilact", ChemicalComposition = "Asparagus racemosus  , Anethum sowa ", TargetAilment = "Gynaecology", DateOfExpiry = Convert.ToDateTime("2022-12-31"),NumberOfTabletsInStock=6000 }
+
+
+
+        };
+
+        public List<MedicineStock> GetAllStokeDetails()
+        {
+            return medicineStock;
+        }
+    }
+}
