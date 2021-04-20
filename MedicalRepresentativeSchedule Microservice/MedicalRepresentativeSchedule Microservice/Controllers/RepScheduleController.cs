6ee8214415eb8d59f9300b036def46ad7871d231
manishjayan <manishjayan@gmail.com>
2021-04-20 16:20:32 +0530

    Added Repository update

--- MedicalRepresentativeSchedule Microservice/MedicalRepresentativeSchedule Microservice/Controllers/RepScheduleController.cs
+++ MedicalRepresentativeSchedule Microservice/MedicalRepresentativeSchedule Microservice/Controllers/RepScheduleController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MedicalRepresentativeSchedule_Microservice.Entity;
+using MedicalRepresentativeSchedule_Microservice.DAL;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Net;
@@ -18,27 +19,21 @@ namespace MedicalRepresentativeSchedule_Microservice.Controllers
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class RepScheduleController : ControllerBase
     {
-        HttpClientHandler handler = new HttpClientHandler();
-        //List Of  Doctor Object Info.
-        List<Doctor> DocList = new List<Doctor>() {
-           new Doctor(){Name="D1",ContactNumber="9884122113",TreatingAlignment="Orthopaedics"},
-           new Doctor(){Name="D2",ContactNumber="9884122113",TreatingAlignment="General"},
-           new Doctor(){Name="D3",ContactNumber="9884122113",TreatingAlignment="General"},
-           new Doctor(){Name="D4",ContactNumber="9884122113",TreatingAlignment="Orthopaedics"},
-           new Doctor(){Name="D5",ContactNumber="9884122113",TreatingAlignment="Gynaecology"}
-        };
+        RepScheduleRespository respository = new RepScheduleRespository();
+
         string[] representatives = new string[] { "R1", "R2", "R3" }; //List of representatives
-     
+
         //Get the Repschedule of 5 days from start Date
         [HttpGet]
         public async Task<IEnumerable<RepSchedule>> Get(DateTime startDate)
         {
-            List<TreatingAlimentMapMedicine> TList = await GetMedicineTreatingAlimentMap();//Return List Of trating aliment with Medicine Name
+            List<Doctor> DocList = respository.GetDoctorList();
+            List<TreatingAlimentMapMedicine> TList = await respository.GetMedicineTreatingAlimentMap();//Return List Of trating aliment with Medicine Name
             List<RepSchedule> RepList = new List<RepSchedule>();
 
             for (int i = 0, j = 0; i < 5; i++, j++)
             {
-                if (j == 3)  
+                if (j == 3)
                 {
                     j = 0;
                 }
@@ -50,11 +45,11 @@ namespace MedicalRepresentativeSchedule_Microservice.Controllers
                     rep.MeetingSlot = "1 PM-2 PM";
                     rep.Dateofmeeting = startDate.Date;
                     rep.Name = representatives[j];
-                    rep.TreatingAlignment = DocList[i].TreatingAlignment;                  
-                    var temp1 = TList.FirstOrDefault(x=>x.TreatingAliment== rep.TreatingAlignment); //Get the List of Medicine of  corresponding rep.TreatingAlignment
+                    rep.TreatingAlignment = DocList[i].TreatingAlignment;
+                    var temp1 = TList.FirstOrDefault(x => x.TreatingAliment == rep.TreatingAlignment); //Get the List of Medicine of  corresponding rep.TreatingAlignment
                     rep.Medicine = temp1.MedicineName;
                     RepList.Add(rep);
-                } 
+                }
                 else
                 {
                     i--;
@@ -66,39 +61,9 @@ namespace MedicalRepresentativeSchedule_Microservice.Controllers
 
             return RepList;
         }
-        //Return List of TreatingAliment and corresponding list of medicine in that aliment
-        [HttpGet("List")]
-        public async Task<List<TreatingAlimentMapMedicine>> GetMedicineTreatingAlimentMap()
-        {
-            List<MedicineStock> stockList = new List<MedicineStock>();
-            List<TreatingAlimentMapMedicine> TreatingAlimentList = new List<TreatingAlimentMapMedicine>();
-            handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-            // handler.UseDefaultCredentials = true;
-           // List<TreatingAlimentMapMedicine> TreatringList = new List<TreatingAlimentMapMedicine>();
-            using (var httpClient = new HttpClient(handler))//handler
-            {
-                //StringContent content = new StringContent(JsonConvert.SerializeObject(medicineDemands), Encoding.UTF8, "application/json");
-                using (var response = await httpClient.GetAsync("http://localhost:5000/api/MedicineStockInformation")) //Call the httpget of MedicineStokeInformation api to fetch  all Medicine stoke info.
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    stockList = JsonConvert.DeserializeObject<List<MedicineStock>>(apiResponse);
-                }
-            }
 
-            foreach (MedicineStock item in stockList)
-            {
-                TreatingAlimentMapMedicine obj = new TreatingAlimentMapMedicine();
-                if (TreatingAlimentList.Find(x => x.TreatingAliment == item.TargetAilment) == null)
-                {
-                    obj.TreatingAliment = item.TargetAilment;
-                    obj.MedicineName = stockList.Where(x => x.TargetAilment == obj.TreatingAliment).Select(y => y.Name).ToList();
-                    TreatingAlimentList.Add(obj);
-                }
-            }
+      
 
-            return TreatingAlimentList;
-        }
-       
-       
+
     }
 }
